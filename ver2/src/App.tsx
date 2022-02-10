8db86c341acbd16688de4179a9cb9423dac602ca
kanghyun98 <lkh981107@gmail.com>
2022-02-10 23:02:14 +0900

    fix css and add location

--- ver2/src/App.tsx
+++ ver2/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Layout from './Layout';
 import Contents from './routes/Contents';
@@ -7,8 +7,10 @@ import Home from './routes/Home';
 import NotFound from './routes/NotFound';
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <Routes>
+    <Routes location={location}>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Contents />} />
